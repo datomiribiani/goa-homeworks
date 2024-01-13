9df87159cc12ad235bea884733bbfcae08cb9046
ggofrex <datomiribi@gmail.com>
2024-01-13 21:56:12 +0400

    first commit

---
+++ day 0/day 0/day0.py
@@ -0,0 +1,39 @@
+name="dato"
+#name არის ცვლადი
+#= არის ცვლადისთვის მნისჰვნელობა მიმჭებელი სიმბოლო
+# "dato" არის ცვლადისთვის მნიშნელობა
+
+surname="miribiani"
+
+#print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "dato" # ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int (ibteger) მთელი რიცხვი
+heigh =168.2  #ეს არის float  ტიპის ცვლადი (ათწილადი)
+#boolean (boll)ტიპის ცვლადი 
+
+knows_programing = Teues # true ან false
+is_ugly = false  #snakecase (უბრადოლ წერის სტილი სტანდარტულად )
+
+isUgly # ჯავასკრიპტული  camelcase 
+
+
+print (name + " " surname)
+#სტრინგი არის ბრჭტყალებში მოცემული სიმბოლოები 
+print (names + age)
+
+print(type(age ))
+print(type( name))
+print(type( surname))
+print(type( heigt))
+print(type(kmows_program))
+
+print(names + " " str(age))
+
+
+my homeworks 
+print((my name dato),+ (my surname miribiani),+ float(my heigt 162.2),+strn (my age 13))
+
+
+my homeworks nubmber 2
