27278dc05adabc38c9cd427e392cc3571ed689d3
ggofrex <datomiribi@gmail.com>
2024-02-11 00:14:23 +0400

    commit

---
+++ day4/day4.py
@@ -0,0 +1,10 @@
+print(იულია დავითისნი,ფერდინანდ მირიბიანი,ნია მირიბიანი,ანაიტა მირიბიანი,ასია რაპიანი)
+\ESFEFeFDFsHFG{pUEFEF
+GFH}ESFEF}ESFHJES}FEFEFFesHFES}
+EFEHF}EHF[fhFFf
+          FE|}SFhES|}FileExistsErrorEF}EHSF
+F\'sF|H}SEFsDWA{}D}WADd
+WJDWdW}DJWDAWDWAD
+WDWDWDWDawd\fefref
+weFNHeFEF
+EFES}{FHESFESfFWADWADWADWADAF}EOFErrorD"WA"DeprecationWar
