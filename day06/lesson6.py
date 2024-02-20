a2d0f8b92165942ff9d8d1a439d86432d59a0b7a
ggofrex <datomiribi@gmail.com>
2024-02-20 19:21:15 +0400

    commit

---
+++ day06/lesson6.py
@@ -0,0 +1,28 @@
+#user_name = input (what is your name?:)
+#print("hello + user_name")
+
+
+
+
+user_name = input("what is your age ? :")
+user_name =("what is your name?:")
+user_name = ("what isyour surname?:")
+
+
+
+print("your name is" + user_name + "your surname is " + user_name + "and you are " + user_age )
+
+
+
+#homework
+
+
+user_name = input("what is your name = dato?:  ")
+user_age =  input("what is your age =13?:")
+user_surname = input("what is your surname =miribiani?:")
+print("your name is" + user_name + "your surname is " + user_surname + "and you are " + user_age )
+
+
+
+
+
