1b3d8e2a0102c0712c6a02547b6340f1c30d4c50
ggofrex <datomiribi@gmail.com>
2024-02-26 20:19:20 +0400

    commit

---
+++ day07/homework.py
@@ -0,0 +1,23 @@
+type = "270"
+print(type)
+
+
+type = "1790"
+print(type)
+
+
+type = "2010"
+print (type)
+
+
+void = 2+4.3
+print( void )
+
+
+void = 190 * 230
+print(void)
+
+
+
+void =37000 - 20000000 
+print(void)  
