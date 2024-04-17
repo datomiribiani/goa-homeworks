325833a85f92676602cdc2e260d28e6f0bc787d6
ggofrex <datomiribi@gmail.com>
2024-04-17 19:59:11 +0400

    commit

---
+++ day22/claswork.py
@@ -0,0 +1,19 @@
+
+# space = ""
+# for i in range (50):
+#      print(space, i)
+
+# while = სანამ
+
+# while 5  > i:
+#     print(i)
+ 
+# academy_name = input("sheiyvane saqartveloshi sauketeso adakemia")
+# while academy_name != 'goa':
+#  academy_name = input("sheiyvane saqartveloshi sauketeso adakemia")
+# print('goa the best')
+
+
+
+# <,>, ==, !=
+ 
