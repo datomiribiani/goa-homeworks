75614ab3bfd60e5096fd12d5110cae5ae4da3f11
ggofrex <datomiribi@gmail.com>
2024-05-21 18:34:14 +0400

    commit

---
+++ day32/classwork.py
@@ -0,0 +1,33 @@
+# goa = "goa the best"
+
+# new_string = ""
+
+# for i in range(12): 
+
+# finals_str =  ""
+# for i in range(10):
+#     print(i)
+
+
+
+# result = ""
+# for i in range(1):
+#     result += "g"
+#     print(result)
+  
+# result = ""    
+
+# for i in range(1):
+
+#     result += "o"
+#     print(result)
+
+
+
+#     result = ""
+
+#     for i in range (1):
+#         result += "a"
+#         print(result)
+
+
---
+++ day32/homework.py
@@ -0,0 +1,9 @@
+goa = "goa s the best"
+
+result = ""
+
+for i in range (11):
+    result =+ [i]
+    print(i)
+
+    print(result)
